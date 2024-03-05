1f240133798901c43e62a96c867ce4c7e734775a
turtle-jin <codingjinjin@gmail.com>
2024-03-04 22:58:10 -0600

    finish desktop design

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-const questions = document.querySelectorAll(".question");
+const sections = document.querySelectorAll(".sections");
 const answers = document.querySelectorAll(".sections>p")
 
-const section1Para = document.querySelector(".section1>p");
-section1Para.style.display = "flex";
 
 
-function toggleAnswers(e) {
-    if (e.style.display = "none") {
-        e.style.display = "flex";
+// Add event listener to each section
+sections.forEach((section) => {
+    section.addEventListener("click", toggleAnswers);
+});
+
+// Function to toggle answers' display
+function toggleAnswers(event) {
+    // Find the clicked section
+    const parentSection = event.currentTarget;
+    console.log(parentSection);
+    // Find the answers within the clicked section
+    const directChildAnswers = parentSection.querySelector(".answers");
+    let icon = parentSection.querySelector("img");
+
+    // Toggle the display property
+    if (directChildAnswers.style.display === "none" || directChildAnswers.style.display === "") {
+        directChildAnswers.style.display = "flex";
+        icon.src = "assets/images/icon-minus.svg";
     } else {
-        e.style.display = "none";
+        directChildAnswers.style.display = "none";
+        icon.src = "assets/images/icon-plus.svg";
     }
 }
-
